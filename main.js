2f57bd7e54cff13769741e350032142f3f1274a8
mamajuice03 <asiswisesa089@gmail.com>
2023-12-02 12:51:26 +0700

    Dek Gittt

---
+++ main.js
@@ -0,0 +1,7 @@
+onload = () =>{
+        document.body.classList.remove("container");
+};
+var audio = document.getElementById("myAudio");
+
+        // Memutar audio secara otomatis
+        audio.play();
